78e6b37345def74194ac622b4c2cfcb9ccae03d3
Cam Sweeney <cpsweene@gmail.com>
2017-02-23 10:10:24 -0800

    fixed imports

--- flask_assistant/__init__.py
+++ flask_assistant/__init__.py
@@ -11,9 +11,7 @@ from flask_assistant.core import (
     context_manager
 )
 
-from flask_assistant.luis.luis import Bot
-from flask_assistant.luis.connector import reply
-from flask_assistant.luis.data_store import DataStore
+from flask_assistant.luis import Bot, reply
 
 from flask_assistant.response import ask, tell
 from flask_assistant.manager import Context
